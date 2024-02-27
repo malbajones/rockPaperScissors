f06c574dd8fb0f5a4f70c1ca932e7572b0a76dbb
malbajones <109672612+malbajones@users.noreply.github.com>
2024-02-27 12:02:09 +0000

    Revisiting Rock Paper Scissors UI.

--- script.js
+++ script.js
@@ -1,49 +1,62 @@
 // Create an Array with the strings: rock, paper and scissors
 const game = ["rock", "paper", "scissors"];
-const player = document.querySelector(".player");
-const computer = document.querySelector(".computer");
-const playerSelection = document.querySelectorAll("button");
+const playerChoice = document.querySelector("#playerChoice");
+const computerChoice = document.querySelector("#computerChoice");
+const gameResults = document.querySelector("#gameResults");
+const playerScoreDisplay = document.querySelector("#playerScoreDisplay");
+const computerScoreDisplay = document.querySelector("#computerScoreDisplay");
+let playScore = 0;
+let computerScore = 0;
+let limit = 5;
 
-//Initialize variables for points counter:
-//a = user, b = computer, and i = while loop counter.
-
-let a = 0;
-let b = 0;
-
-//Function to randomly select computer's choice
-const getComputerChoice = () => {
-  randomChoice = game[Math.floor(Math.random() * game.length)];
-  return randomChoice;
-};
-
-//Function to play the game with two parameters: user's choice and
-//computer's choice
-const playGame = (playerSelection, computerSelection) => {
-  //Check for a tie
-  if (playerSelection === computerSelection) {
-    console.log("Tie");
-    a++;
-    b++;
-  } else if (
-    //Check for user wins
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    console.log("User is the Winner");
-    a++;
+function playGame(playerOption) {
+  const computerOption = game[Math.floor(Math.random() * 3)];
+  let result = "";
+  if (playerOption === computerOption) {
+    result = "It's a Tie!";
   } else {
-    // Computer wins
-    console.log("Computer is the winner");
-    b++;
+    switch (playerOption) {
+      case "rock":
+        result = computerOption === "scissors" ? "You Win!" : "You Lose";
+        break;
+      case "paper":
+        result = computerOption === "rock" ? "You Win!" : "You Lose";
+        break;
+      case "scissors":
+        result = computerOption === "paper" ? "You Win!" : "You Lose";
+        break;
+    }
   }
-  player.textContent = "User points: " + a;
-  computer.textContent = " Computer points: " + b;
-  console.log("User points: " + a + " Computer points: " + b);
-};
+  playerChoice.textContent = `Player: ${playerOption}`;
+  computerChoice.textContent = `Computer: ${computerOption}`;
+  gameResults.textContent = result;
+  gameResults.classList.remove("greenTex", "orangeText");
 
-// Function to play the game when a button is clicked
-function play(playerChoice) {
-  const computer = getComputerChoice(); // Get computer's choice
-  playGame(playerChoice, computer); // Play the game with the player's choice and computer's choice
+  switch (result) {
+    case "You Win!":
+      gameResults.classList.add("greenTex");
+      playScore++;
+      playerScoreDisplay.textContent = playScore;
+      break;
+    case "You Lose":
+      gameResults.classList.add("orangeText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
+  }
+  if (playScore === limit || computerScore === limit) {
+    endGame();
+  }
+}
+
+function endGame() {
+  if (playScore === computerScore) {
+    gameResults.textContent = "It's a Tie!";
+  } else if (playScore > computerScore) {
+    gameResults.textContent = "You win the game!";
+  } else {
+    gameResults.textContent = "computer win the game!";
+  }
+  playScore = 0;
+  computerScore = 0;
 }
